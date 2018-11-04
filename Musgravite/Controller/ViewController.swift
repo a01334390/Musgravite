f93569b5c8544ee95993653019cebb56de324bc5
Fernando Martín García Del Angel <martingarciadelangel@me.com>
2018-11-04 01:36:32 -0600

    Added BLTN Page
    This application now supports BLTNPages

--- Musgravite/Controller/ViewController.swift
+++ Musgravite/Controller/ViewController.swift
@@ -7,17 +7,22 @@
 //
 
 import UIKit
-import OnboardKit
+import BLTNBoard
 
 class ViewController: UIViewController {
     /* This will allow Haptic Feedback to work */
     let impact = UIImpactFeedbackGenerator()
     let notification = UINotificationFeedbackGenerator()
     let selection = UISelectionFeedbackGenerator()
+    /* Bulletin board */
+    lazy var bulletinManager: BLTNItemManager = {
+        let introPage = createOnboardingExperience()
+        return BLTNItemManager(rootItem: introPage)
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
     
     /**
@@ -38,34 +43,59 @@ class ViewController: UIViewController {
     
     /**
        This launches the onboarding experience on first launch
-     - Returns: nil
+     - Returns: OnboardingBLTNPageItem
      - Remark: After the onboarding has completed, the app will not show the experience anymore
      - Requires: The application to be launched for the first time and the process to have completed
      */
-    func launchOnboardingExperience(){
-        let page = OnboardPage(title: "Welcome to OnboardKit",
-                               imageName: "Onboarding1",
-                               description: "OnboardKit helps you add onboarding to your iOS app")
-        let pageFour = OnboardPage(title: "Notifications",
-                                   imageName: "Onboarding4",
-                                   description: "Turn on notifications to get reminders and keep up with your goals.",
-                                   advanceButtonTitle: "Decide Later",
-                                   actionButtonTitle: "Enable Notifications",
-                                   action: { [weak self] completion in
-                                    print("wow")})
-        let pageFive = OnboardPage(title: "All Ready",
-                                   imageName: "buletin-1",
-                                   description: "You are all set up and ready to use Habitat. Begin by adding your first habit.",
-                                   advanceButtonTitle: "Done")
-
-        let onboardingViewController = OnboardViewController(pageItems: [page,pageFour,pageFive])
-            onboardingViewController.presentFrom(self, animated: true)
-        
+    func createOnboardingExperience() -> BLTNPageItem {
+        let firstPage = BLTNPageItem(title: "Bienvenido a Musgravite")
+        firstPage.image = UIImage(named: "buletin-1")
+        firstPage.descriptionText = "Descubre los laboratorios que existen en CEDETEC y crea tu siguiente innovacion"
+        firstPage.actionButtonTitle = "Configurar"
+        firstPage.alternativeButtonTitle = "Ahora no"
+        firstPage.requiresCloseButton = false
+        firstPage.isDismissable = false
+        firstPage.next = createLocationServicesBLTNPage()
+        firstPage.actionHandler = { item in
+            self.selection.selectionChanged()
+            item.manager?.displayNextItem()
+        }
+        firstPage.alternativeHandler = { item in
+            item.manager?.dismissBulletin(animated: true)
+        }
+        return firstPage
+    }
+    
+    /**
+     This launches the location services BLTN Page
+     - Returns: OnboardingBLTNPageItem
+     - Remark: This should call our support file for location services
+     - Requires: It requires to be called by another BTLNPage and is not the first page to show
+     */
+    func createLocationServicesBLTNPage() -> BLTNPageItem {
+        let firstPage = BLTNPageItem(title: "Servicios de Localizacion")
+        firstPage.image = UIImage(named: "buletin-2")
+        firstPage.descriptionText = "Para personalizar tu experiencia necesitamos tu localizacion. Esta informacion sera enviada a nuestros servidores de forma anonima. Puedes cambiar de opinion mas adelante en las preferencias de la aplicacion."
+        firstPage.actionButtonTitle = "Activar Localizacion"
+        firstPage.alternativeButtonTitle = "Ahora no"
+        firstPage.requiresCloseButton = false
+        firstPage.isDismissable = false
+        firstPage.appearance.shouldUseCompactDescriptionText = true
+        firstPage.actionHandler = { item in
+            item.manager?.dismissBulletin()
+        }
+        firstPage.alternativeHandler = { item in
+            item.manager?.dismissBulletin(animated: true)
+        }
+        return firstPage
     }
     
     override func viewDidAppear(_ animated: Bool) {
         if (appHasBeenLaunchedBefore()){
-            launchOnboardingExperience()
+            /* Launch Onboarding */
+            notification.notificationOccurred(.warning)
+            bulletinManager.backgroundViewStyle = .blurredLight
+            bulletinManager.showBulletin(above: self)
         }
     }
     
