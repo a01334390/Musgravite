775185414596dd5e407cfff1a3c8d39a2de359bf
Armando Hernandez <armandohernandez@MacBook-Pro-de-Armando-2.local>
2018-11-20 15:14:36 -0600

    added ml and 360view

--- Musgravite/Controller/ARControllers/ARDomeViewController.swift
+++ Musgravite/Controller/ARControllers/ARDomeViewController.swift
@@ -10,15 +10,24 @@ import UIKit
 import SceneKit
 import ARKit
 import SwiftMessages
+import Vision
 
 class ARDomeViewController: UIViewController, ARSCNViewDelegate {
     var panonoImage:UIImage?
-    
+    private var hitTestResult: ARHitTestResult!
+    private var resnetModel = Resnet50()
+    private var visionRequests = [VNRequest]()
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //MLsetup
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        let scene = SCNScene()
+        sceneView.scene = scene
+        
         //mostrar el origen y los puntos detectados
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         //indicar la detección del plano
@@ -120,6 +129,116 @@ class ARDomeViewController: UIViewController, ARSCNViewDelegate {
         
         // Show the message.
         SwiftMessages.show(config: config, view: view)
+    }
+    
+    //ML stuff
+    
+    @IBAction func tapEjecutado(_ sender: UITapGestureRecognizer) {
+        let vista = sender.view as! ARSCNView
+        let ubicacionToque = self.sceneView.center
+        guard let currentFrame = vista.session.currentFrame else {return}
+        let hitTestResults = vista.hitTest(ubicacionToque, types: .featurePoint)
+        if (hitTestResults .isEmpty){
+            return}
+        guard var hitTestResult = hitTestResults.first else{
+            return
+        }
+        let imagenPixeles = currentFrame.capturedImage
+        self.hitTestResult = hitTestResult
+        performVisionRequest(pixelBuffer: imagenPixeles)
+    }
+    
+    private func performVisionRequest(pixelBuffer: CVPixelBuffer){
+        //inicializar el modelo de ML al modelo usado, en este caso resnet
+        let visionModel = try! VNCoreMLModel(for: resnetModel.model)
+        let request = VNCoreMLRequest(model: visionModel) { request, error in
+            
+            if error != nil {
+                //hubo un error
+                return}
+            guard let observations = request.results else {
+                //no hubo resultados por parte del modelo
+                return
+                
+            }
+            //obtener el mejor resultado
+            let observation = observations.first as! VNClassificationObservation
+            
+            print("Nombre \(observation.identifier) confianza \(observation.confidence)")
+            self.desplegarTexto(entrada: observation.identifier)
+            
+        }
+        //la imagen que se pasará al modelo sera recortada para quedarse con el centro
+        request.imageCropAndScaleOption = .centerCrop
+        self.visionRequests = [request]
         
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: [:])
+        DispatchQueue.global().async {
+            try! imageRequestHandler.perform(self.visionRequests)
+            
+        }
+    }
+    
+    private func desplegarTexto(entrada: String){
+        
+        let view = MessageView.viewFromNib(layout: .cardView)
+        
+        // Theme message elements with the warning style.
+        view.configureTheme(.info)
+        
+        // Add a drop shadow.
+        view.configureDropShadow()
+        
+        // Set message title, body, and icon. Here, we're overriding the default warning
+        // image with an emoji character.
+        let iconText = ["👀"].sm_random()!
+        view.configureContent(title: "Estas viendo", body: entrada, iconText: iconText)
+        
+        // Increase the external margin around the card. In general, the effect of this setting
+        // depends on how the given layout is constrained to the layout margins.
+        view.layoutMarginAdditions = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+        // Reduce the corner radius (applicable to layouts featuring rounded corners).
+        (view.backgroundView as? CornerRoundingView)?.cornerRadius = 10
+        var config = SwiftMessages.Config()
+        config.presentationContext = .window(windowLevel: .statusBar)
+        config.duration = .forever
+        
+        // Show the message.
+        SwiftMessages.show(config: config, view: view)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
     }
 }
--- Musgravite/Controller/ARControllers/P360ViewController.swift
+++ Musgravite/Controller/ARControllers/P360ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import SceneKit
 import CTPanoramaView
-
+import SwiftMessages
+import Vision
+import ARKit
 
 class P360ViewController: UIViewController {
-    var panonoImageURL:URL?
+    var panonoImage:UIImage?
+    private var hitTestResult: ARHitTestResult!
+    private var resnetModel = Resnet50()
+    private var visionRequests = [VNRequest]()
     @IBOutlet weak var compassView: CTPieSliceView!
     @IBOutlet weak var pv: CTPanoramaView!
     
@@ -40,27 +46,109 @@ class P360ViewController: UIViewController {
         }
     }
     
-    private func load(fileURL: URL) -> UIImage? {
-        do {
-            let imageData = try Data(contentsOf: fileURL)
-            return UIImage(data: imageData)
-        } catch {
-            print("Error loading image : \(error)")
-        }
-        return nil
-    }
-    
     func loadSphericalImage() {
-        pv.image = load(fileURL: panonoImageURL!)
+        pv.image = panonoImage
     }
     
     func loadCylindricalImage() {
-        pv.image = load(fileURL: panonoImageURL!)
+        pv.image = panonoImage
     }
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .all
     }
-
-
+    
+    //ML stuff
+    
+    @IBAction func tapEjecutado(_ sender: UITapGestureRecognizer) {
+        let vista = sender.view as! ARSCNView
+        let ubicacionToque = self.pv.center
+        guard let currentFrame = vista.session.currentFrame else {return}
+        let hitTestResults = vista.hitTest(ubicacionToque, types: .featurePoint)
+        if (hitTestResults .isEmpty){
+            return}
+        guard var hitTestResult = hitTestResults.first else{
+            return
+        }
+        let imagenPixeles = currentFrame.capturedImage
+        self.hitTestResult = hitTestResult
+        performVisionRequest(pixelBuffer: imagenPixeles)
+    }
+    
+    private func performVisionRequest(pixelBuffer: CVPixelBuffer){
+        //inicializar el modelo de ML al modelo usado, en este caso resnet
+        let visionModel = try! VNCoreMLModel(for: resnetModel.model)
+        let request = VNCoreMLRequest(model: visionModel) { request, error in
+            
+            if error != nil {
+                //hubo un error
+                return}
+            guard let observations = request.results else {
+                //no hubo resultados por parte del modelo
+                return
+                
+            }
+            //obtener el mejor resultado
+            let observation = observations.first as! VNClassificationObservation
+            
+            print("Nombre \(observation.identifier) confianza \(observation.confidence)")
+            self.desplegarTexto(entrada: observation.identifier)
+            
+        }
+        //la imagen que se pasará al modelo sera recortada para quedarse con el centro
+        request.imageCropAndScaleOption = .centerCrop
+        self.visionRequests = [request]
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: [:])
+        DispatchQueue.global().async {
+            try! imageRequestHandler.perform(self.visionRequests)
+            
+        }
+    }
+    
+    private func desplegarTexto(entrada: String){
+        
+        let view = MessageView.viewFromNib(layout: .cardView)
+        
+        // Theme message elements with the warning style.
+        view.configureTheme(.info)
+        
+        // Add a drop shadow.
+        view.configureDropShadow()
+        
+        // Set message title, body, and icon. Here, we're overriding the default warning
+        // image with an emoji character.
+        let iconText = ["👀"].sm_random()!
+        view.configureContent(title: "Estas viendo", body: entrada, iconText: iconText)
+        
+        // Increase the external margin around the card. In general, the effect of this setting
+        // depends on how the given layout is constrained to the layout margins.
+        view.layoutMarginAdditions = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+        // Reduce the corner radius (applicable to layouts featuring rounded corners).
+        (view.backgroundView as? CornerRoundingView)?.cornerRadius = 10
+        var config = SwiftMessages.Config()
+        config.presentationContext = .window(windowLevel: .statusBar)
+        config.duration = .forever
+        
+        // Show the message.
+        SwiftMessages.show(config: config, view: view)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    }
 }
